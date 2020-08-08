05d09e734ae8afedb9199666ec3afec70a266031
Anh Dinh Trong <68251024+ninelie72@users.noreply.github.com>
2020-08-09 01:54:51 +0200

    Add files via upload
    initial fork

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+
+import datetime
+
+#Fetch original app token for security reasons.
+token = input('Please enter the sacred adage: ')    
+
+#Define said bot, Shinigami as client.
+client = commands.Bot(command_prefix = '#')     
+
+#Making sure everything's online and ready.
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+#Owner's key.
+def permission_check(ctx):
+    return ctx.author.id == ctx.guild.owner_id 
+
+#Purge command.
+@client.command()
+@commands.check(permission_check)
+async def purge(ctx):
+    channel = ctx.message.channel
+    date = ctx.message.created_at - datetime.timedelta(30)
+    guild = ctx.guild
+    print(date)
+    role = discord.utils.find(lambda i: i.id == 737360924276686929, guild.roles)
+    active_users = dict()
+
+    async for message in channel.history(limit = None, after = date):
+        if message.author in guild.members:
+            if (not role in message.author.roles and not message.author.bot):
+                if not message.author.display_name in active_users:
+                    active_users[message.author.display_name] = 1
+                else:
+                    active_users[message.author.display_name] += 1
+    for x, y in active_users.items(): 
+            await ctx.send(f'{x}: {y}')
+
+    for member in guild.members:
+        if (not role in member.roles and not member.bot 
+                and not member.display_name in active_users):
+            await ctx.send(f'Fuck you, {member.display_name}!')
+            await member.kick()
+        
+#Run the bot.
+client.run(token)
